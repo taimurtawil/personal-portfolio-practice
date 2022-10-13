a5e0ccdb53dee606a5a8a465e694f4e381f93c76
Taimur Tawil <taimurtawil@gmail.com>
2022-10-13 16:56:48 -0400

    NavBar and Packages

---
+++ src/components/Layout/index.js
@@ -0,0 +1,12 @@
+import NavBar from '../NavBar'
+import './index.scss'
+
+const Layout = () => {
+    return (
+        <>
+            <NavBar/>
+        </>
+    )
+}
+
+export default Layout
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,32 @@
+import './index.scss'
+import { NavLink, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons'
+
+const NavBar = () => {
+    //TODO: make logo 
+
+    return(
+        <div className = 'nav-bar'>
+            <Link className = 'logo' to='/'>
+                Home
+                
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e"></FontAwesomeIcon>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e"></FontAwesomeIcon>
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
+                </NavLink>
+            </nav>
+
+        </div>
+
+    )
+}
+
+export default NavBar
